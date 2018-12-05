7fde568888634e3f2aa02098f126cbd0e0e8ce08
Heather <unphurlng@gmail.com>
2018-12-04 18:03:55 -0800

    data added in postman registering as expected in server console

--- server/server.js
+++ server/server.js
@@ -7,13 +7,29 @@ function readData() {
   return JSON.parse(data);
 }
 
-// app.use(express.json());
+function saveData(campgrounds) {
+  const json = JSON.stringify(campgrounds, true, 2);
+  fs.writeFileSync('./data/campgrounds.json', json);
+}
+
+app.use(express.json());
 
 app.get('/api/campgrounds', (req, res) => {
   const campgrounds = readData();
   res.json(campgrounds);
 });
 
+app.post('/api/campgrounds', (req, res) => {
+
+  console.log('POST campgrounds received', req.body);
+  // const campgrounds = readData();
+
+  // campgrounds.push(campground);
+
+  // saveData(campgrounds);
+
+  res.json({});
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
