115214e3dcfb67f84126364d6e6adc58523bffb3
Heather <unphurlng@gmail.com>
2018-12-06 15:12:47 -0800

    added campgroundList.vue

---
+++ app/src/components/campgrounds/CampgroundList.vue
@@ -0,0 +1,19 @@
+<template>
+  <ul v-if="campgrounds">
+    <li v-for="campground in campgrounds" :key="campground.id">
+      {{campground.name}}
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    campgrounds: null
+  }
+};
+</script>
+
+<style>
+
+</style>
