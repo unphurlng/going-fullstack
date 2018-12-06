6a57df484412403a7af2619800ba2f1ea4292820
Heather <unphurlng@gmail.com>
2018-12-06 15:05:54 -0800

    added campground detail

--- app/src/components/campgrounds/AddCampground.vue
+++ app/src/components/campgrounds/AddCampground.vue
@@ -1,21 +1,61 @@
 <template>
   <form @submit.prevent="handleSubmit">
-    <input v-model="campground.name" require>
+    <p>
+      <label>Name:</label>
+      <input v-model="campground.name" require>
+    </p>
+
+    <p>
+      <label>Forest:</label>
+      <select v-if="forest"
+        v-model="campground.forest"
+        required
+      >
+        <option v-for="(display, key) in forest"
+          :key="key"
+          :value="key"
+        >
+          {{display}}
+        </option>
+      </select>
+    </p>
+
+    <p>
+      <label>Season Open:</label>
+      <input v-model="campground.seasonDates">
+    </p>
+
+    <p>
+      <label>Number of Sites:</label>
+      <input v-model="campground.sites" type="number">
+    </p>
+
     <button>Add</button>
   </form>
 </template>
 
 <script>
+import api from '../../services/api';
+
 export default {
   props: {
     onAdd: Function,
   },
   data() {
     return {
-      campground: {
-        name: ''
-      }
+      name: '',
+      forest: '',
+      seasonDates: '',
+      sites: 0,
     };
+  },
+  created() {
+    this.forest = api.getForest();
+  },
+  methods: {
+    handleSubmit() {
+      this.onAdd(this.campground);
+    }
   }
 };
 </script>
---
+++ app/src/components/campgrounds/CampgroundDetail.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h2>{{campground.name}}</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    campgrounds: null
+  }
+};
+</script>
+
+<style>
+
+</style>
