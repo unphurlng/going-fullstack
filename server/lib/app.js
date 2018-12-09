ebcb72ddea5842bf3c39ad0574302265861fe897
Heather <unphurlng@gmail.com>
2018-12-08 23:53:09 -0800

    split server.js into app part and server part for starting server

---
+++ server/lib/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const campgrounds = require('./routes/campgrounds');
+const forests = require('./routes/forests');
+
+
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api/forests', forests);
+app.use('/api/campgrounds', campgrounds);
+
+module.exports = app;
