ba108962cc309df2c0753aa342675dfeee6f8911
Heather <unphurlng@gmail.com>
2018-12-08 23:16:37 -0800

    move campground routest to campground.js file

---
+++ server/lib/routes/campgrounds.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const client = require('../../db-client');
+const Router = express.Router;
+const router = Router(); //eslint-disable-line new-cap
+
+router
+  .get('/api/campgrounds', (req, res) => {
+    client.query(`
+      SELECT id, name, forest, sites, rvwaste
+      FROM campgrounds
+      ORDER BY name;
+    `)
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  })
+
+  .get('/api/campgrounds/:id', (req, res) => {
+    client.query(`
+      SELECT * FROM campgrounds WHERE id = $1;
+    `,
+    [req.params.id])
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  })
+
+  .post('/api/campgrounds', (req, res) => {
+    const body = req.body;
+
+    client.query(`
+      INSERT INTO campgrounds (name, forest, sites, rvwaste)
+      VALUES($1, $2, $3, $4)
+      RETURNING id, name, forest, sites, rvwaste;
+    `,
+    [body.name, body.forest, body.sites, body.rvwaste])
+      .then(result => {
+        res.json(result.rows[0]);
+      });
+  });
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 const morgan = require('morgan');
+const campgrounds = require('./lib/routes/campgrounds');
 const client = require('./db-client');
 
 app.use(morgan('dev'));
 app.use(express.json());
+
 
 // Connect to pg
 // const Client = pg.Client;
@@ -23,29 +25,32 @@ app.get('/api/campgrounds', (req, res) => {
     });
 });
 
-app.get('/api/campgrounds/:id', (req, res) => {
-  client.query(`
-    SELECT * FROM campgrounds WHERE id = $1;
-  `,
-  [req.params.id])
-    .then(result => {
-      res.json(result.rows[0]);
-    });
-});
+// app.get('/api/campgrounds/:id', (req, res) => {
+//   client.query(`
+//     SELECT * FROM campgrounds WHERE id = $1;
+//   `,
+//   [req.params.id])
+//     .then(result => {
+//       res.json(result.rows[0]);
+//     });
+// });
 
-app.post('/api/campgrounds', (req, res) => {
-  const body = req.body;
+// app.post('/api/campgrounds', (req, res) => {
+//   const body = req.body;
 
-  client.query(`
-    INSERT INTO campgrounds (name, forest, sites, rvwaste)
-    VALUES($1, $2, $3, $4)
-    RETURNING id, name, forest, sites, rvwaste;
-  `,
-  [body.name, body.forest, body.sites, body.rvwaste])
-    .then(result => {
-      res.json(result.rows[0]);
-    });
-});
+//   client.query(`
+//     INSERT INTO campgrounds (name, forest, sites, rvwaste)
+//     VALUES($1, $2, $3, $4)
+//     RETURNING id, name, forest, sites, rvwaste;
+//   `,
+//   [body.name, body.forest, body.sites, body.rvwaste])
+//     .then(result => {
+//       res.json(result.rows[0]);
+//     });
+// });
+
+app.use(campgrounds);
+// const app = require('./lib/app');
 
 const PORT = 3000;
 
